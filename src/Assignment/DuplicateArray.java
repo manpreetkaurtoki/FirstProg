e2d868789ce7d7641288bb83714a570b1640f98a
Manpreet <manpreetkaurtoki@gmail.com>
2026-04-18 18:01:08 -0400

    Java program array practice

---
+++ src/Assignment/DuplicateArray.java
@@ -0,0 +1,36 @@
+package Assignment;
+
+public class DuplicateArray {
+
+	public static void main(String arg[]) {
+		// find duplicate element in array
+
+		int ar[] = { 1, 4, 5, 8, 9, 0, 3, 1, 2, 3, 4, 8 };
+		for (int i = 0; i < ar.length; i++) {
+			for (int j = i + 1; j < ar.length; j++) {
+				if (ar[i] == ar[j]) {
+					System.out.println("Duplicate: " + ar[i]);
+					break;
+				}
+			}
+
+		}
+
+		// smallest and largest element in array
+		int arr[] = { 9, 3, 4, 2, 1, 4, 8 };
+		int smallest = arr[0];
+		int largest = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > largest) {
+				largest = arr[i];
+
+			}
+			if (arr[i] <smallest) {
+				smallest = arr[i];
+			}
+		}
+		System.out.println("largest number in array is : " + largest);
+		System.out.println("smallest number in array is : " + smallest);
+
+	}
+}

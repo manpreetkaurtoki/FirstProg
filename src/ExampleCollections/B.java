b73166d5c458e375f3a7714ff57c4a57008fc9f2
Manpreet <manpreetkaurtoki@gmail.com>
2026-04-28 17:39:03 -0400

    practice java

--- src/ExampleCollections/B.java
+++ src/ExampleCollections/B.java
@@ -1,25 +1,38 @@
 package ExampleCollections;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
-class A
-{
-	public void hello()
-	{
+class A {
+	public void hello() {
 		System.out.println("HelloA");
 	}
 }
-class B extends A
-{
-	public void hello()
-	{
+
+class B extends A {
+	public void hello() {
 		System.out.println("HelloB");
-	
+		Set<Integer> myset = new HashSet<Integer>();
+		myset.add(1);
+		myset.add(1);
+		myset.add(3);
+		myset.add(4);
+		myset.add(3);
+		myset.add(1);
+		// myset.add(null);
+
+		TreeSet<Integer> mytree = new TreeSet<Integer>(myset);
+		mytree.addAll(myset);
+		System.out.println("Size of set" + mytree.size());
+		System.out.println("Size of set" + myset.size());
 	}
+
 	public static void main(String[] ard)
 
 	{
-		B obj= new B();
+		B obj = new B();
 		obj.hello();
 	}
-	
+
 }
